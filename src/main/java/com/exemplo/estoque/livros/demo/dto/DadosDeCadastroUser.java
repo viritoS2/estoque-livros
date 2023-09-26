7e49a72cecf8f975179c6292d57a8ac40663e7b9
vitor-amorim <vitoramorimof@gmail.com>
2023-09-26 15:05:39 -0300

    configurando rotas e schemas para o swagger ui

--- src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCadastroUser.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCadastroUser.java
@@ -1,3 +1,6 @@
 package com.exemplo.estoque.livros.demo.dto;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
 public record DadosDeCadastroUser(Long id, String nome, String email) {}
--- src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCasdastroLivro.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCasdastroLivro.java
@@ -1,3 +1,6 @@
 package com.exemplo.estoque.livros.demo.dto;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
 public record DadosDeCasdastroLivro(Long id, String nome, String autor, Long quantidade) {}
