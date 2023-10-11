d0a8c7d4e7b1e30ee2fe70b11d7c404fdcc4a8b7
vitor-amorim <vitoramorimof@gmail.com>
2023-10-11 13:52:26 -0300

    add sales

---
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCadastroSales.java
@@ -0,0 +1,8 @@
+package com.exemplo.estoque.livros.demo.dto;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+import java.util.UUID;
+
+@Hidden
+public record DadosDeCadastroSales(UUID id, Long book_id, Long user_id,Long quantity, Long total_cost){}
--- src/main/java/com/exemplo/estoque/livros/demo/dto/Sales.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/Sales.java
@@ -1,4 +1,37 @@
 package com.exemplo.estoque.livros.demo.dto;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class Sales {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "book_id")
+    private Long book_id;
+
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(name = "sales_date")
+    private Long sales_date;
+
+    @Column(name = "quantity")
+    private Long quantity;
+
+    @Column(name = "total_cost")
+    private Long total_cost;
+
+    private Date  now = new Date();
+
+    public Sales(DadosDeCadastroSales dados){
+        this.id = dados.id();
+        this.book_id = dados.book_id();
+        this.user_id =dados.user_id();
+        this.sales_date = now.getTime();
+        this.quantity = dados.quantity();
+        this.total_cost = dados.total_cost();
+    }
 }
