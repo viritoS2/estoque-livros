341ae2eb4d693c875cecd4d0464f4effafea14cd
vitor-amorim <vitoramorimof@gmail.com>
2023-10-11 13:52:05 -0300

    add new migrations

--- src/main/resources/db/migration/V2__create_table_sales.sql
+++ src/main/resources/db/migration/V2__create_table_sales.sql
@@ -1,1 +1,11 @@
-CREATE TABLE foods(nome varchar(255))
+CREATE TABLE sales (
+  id INT NOT NULL AUTO_INCREMENT,
+  book_id INT NOT NULL,
+  user_id INT NOT NULL,
+  sales_date DATETIME NOT NULL,
+  quantity INT NOT NULL,
+  total_cost DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (book_id) REFERENCES livros (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+)
