1cbdfe57db510cba38dd0f0975bc12089b381778
vitor-amorim <vitoramorimof@gmail.com>
2023-10-25 18:30:35 -0300

    add uuid in record class sales

--- src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCadastroSales.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/DadosDeCadastroSales.java
@@ -5,4 +5,4 @@ import io.swagger.v3.oas.annotations.Hidden;
 import java.util.UUID;
 
 @Hidden
-public record DadosDeCadastroSales(UUID id, Long book_id, Long user_id,Long quantity, Long total_cost){}
+public record DadosDeCadastroSales(Long book_id, Long user_id,Long quantity, Long total_cost){}
