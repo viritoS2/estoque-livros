7ee7d52d857c280c3e5fa5c98e1c8a65dbfec855
viritoS2 <vitoramorimof@gmail.com>
2024-02-08 17:54:52 -0300

    refactor properties of sales

--- src/main/java/com/exemplo/estoque/livros/demo/dto/Sales.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/Sales.java
@@ -13,14 +13,14 @@ public class Sales {
     @Column(name = "uuid")
     private Long uuid;
     @Column(name = "book_id")
-    private Long book_id;
+    private Long bookId;
 
     @Column(name = "user_id")
-    private Long user_id;
+    private Long userId;
 
     @Column(name = "sales_date")
     @Temporal(TemporalType.DATE)
-    private Date sales_date;
+    private Date salesDate;
 
     @Column(name = "quantity")
     private Long quantity;
@@ -30,9 +30,9 @@ public class Sales {
 
 
     public Sales(DadosDeCadastroSales dados){
-        this.book_id = dados.book_id();
-        this.user_id =dados.user_id();
-        this.sales_date = new Date();
+        this.bookId = dados.book_id();
+        this.userId =dados.user_id();
+        this.salesDate = new Date();
         this.quantity = dados.quantity();
         this.total_cost = dados.total_cost();
     }
