633372f77a89baa26bb471fac74551e03873030f
vitor <vitoramorimof@gmail.com>
2024-06-01 14:02:05 -0300

    add getters and setters

--- src/main/java/com/exemplo/estoque/livros/demo/dto/User.java
+++ src/main/java/com/exemplo/estoque/livros/demo/dto/User.java
@@ -9,30 +9,49 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "nome")
-    private String nome;
+    @Column(name = "name")
+    private String name;
     @Column(name = "email")
     private String email;
 
 
     public User (DadosDeCadastroUser dados){
         this.id = dados.id();
-        this.nome = dados.nome();
+        this.name = dados.name();
         this.email = dados.email();
     }
 
     public User(){}
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
        return  "Usuario{ " +
                 "id=" + this.id + '\'' +
-                "nome=" + this.nome + '\'' +
+                "nome=" + this.name + '\'' +
                 "email=" + this.email + '\'' +
                 '}';
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
