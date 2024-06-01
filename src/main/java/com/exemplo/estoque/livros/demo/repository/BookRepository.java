bf893315e59a7eec73cfce202078b3b445f31217
vitor <vitoramorimof@gmail.com>
2024-06-01 00:01:02 -0300

    rename interface name livrorepository to bookrepository

--- src/main/java/com/exemplo/estoque/livros/demo/repository/BookRepository.java
+++ src/main/java/com/exemplo/estoque/livros/demo/repository/BookRepository.java
@@ -1,6 +1,6 @@
 package com.exemplo.estoque.livros.demo.repository;
 
-import com.exemplo.estoque.livros.demo.dto.Livro;
+import com.exemplo.estoque.livros.demo.dto.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LivroRepository extends JpaRepository<Livro, Long> {}
+public interface BookRepository extends JpaRepository<Book, Long> {}
