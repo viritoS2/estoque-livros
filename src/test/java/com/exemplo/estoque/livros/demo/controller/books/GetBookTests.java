0592353dc79896e0e3504fba84d7386b798e07d7
vitor <vitoramorimof@gmail.com>
2024-06-01 00:02:56 -0300

    add get book test

--- src/test/java/com/exemplo/estoque/livros/demo/controller/books/GetBookTests.java
+++ src/test/java/com/exemplo/estoque/livros/demo/controller/books/GetBookTests.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
-public class BookControllerTests {
+public class GetBookTests {
 
     @Autowired
     private MockMvc mockMvc;
